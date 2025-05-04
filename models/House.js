99ad59c0763d3347bd2c28bebed838c0bd210019
Vishwa Badrinadh Padala <vishwabadrinadh.padala@gmail.com>
2025-05-04 05:50:12 +0530

    AvailableFrom

--- models/House.js
+++ models/House.js
@@ -54,7 +54,7 @@ const HouseSchema = new mongoose.Schema({
     features: [{ type: String }],
     amenities: [{ type: String }],
     labels: [{ type: String }],
-    availableFrom: { type: Date, required: true, default: Date.now },
+    availableFrom: { type: Date, default: Date.now },
     isFeatured: { type: Boolean, default: false },
     isActive: { type: Boolean, default: true },
     createdBy: { type: String },
